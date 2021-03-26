37ee64594d1556dcc16b68fe62d3887cced9f8a7
unknown <hemanthsuryavj0@gmail.com>
2021-03-26 09:00:02 +0530

    object oriented

--- Lc.java
+++ Lc.java
@@ -5,43 +5,49 @@ public class Lc
 
 	{
 
-             	 int x1,x2,y1,y2;
+            double line1distance = DistanceBwtTwoPoints();
+            System.out.println("Distance between the two points::" + line1distance);
+            double line2Distance = DistanceBwtTwoPoints();
+            System.out.println("Distance between two points::" + line2Distance);
+            string compareTwoLines = CompareTwoLines(line1distance,line2Distance);
+            System.out.println(compareTwoLines);
+         }
 
-	         double dis;
-		 string compareTwoLines = CompareTwoLines(line1,line2);
-            	 System.out.println("compareTwoLines");
+	 public static double GetEndPointsOfX()
+         {
+	    Scanner sc = new Scanner(System.in);
+            System.out.println("Enter X1");
+            x1 = sc.nextDouble();
+            System.out.println("enter x2");
+	    x2 = sc.nextDouble();
+            return Math.Pow((x2 - x1), 2);
+         }
+	 
+	 public static double GetEndPointsOfY()
+         {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter Y1");
+            y1 = sc.nextDouble();
+            System.out.println("enter Y2");
+            y2 = sc.nextDouble();
+            return Math.Pow((y2 - y1), 2);
+         }
 
-	         Scanner sc=new Scanner(System.in);
+	 public static double DistanceBwtTwoPoints()
+         {
+            double xCoordinates = GetEndPointsOfX();
+            double yCoordinates = GetEndPointsOfY();
+            double distance=Math.Sqrt(xCoordinates + yCoordinates);
+            return Math.Round(distance, 2);
+         }
 
-	         System.out.println("enter x1 point");
-
-                 x1=sc.nextInt();
-
-                 System.out.println("enter y1 point");
-
-                 y1=sc.nextInt();
-
-	         System.out.println("enter x2point");
-
-                 x2=sc.nextInt();
-
-	         System.out.println("enter y2 point");
-
-                 y2=sc.nextInt();
-
-		 dis=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-
-  	         System.out.println("distancebetween"+"("+x1+","+y1+"),"+"("+x2+","+y2+")="+dis);
-
-	}
-
-
-	public static string CompareTwoLines(double line1,double line2)
-        {
+	 public static string CompareTwoLines(double line1,double line2)
+         {
             int checkLines = line1.CompareTo(line2);
-            System.println("checkLines");
+            System.out.println("Checklines");
             return checkLines == 0 ? "Both lines are equal" :
-                   checkLines == -1 ? "First Line is less Than Second Line" :"First Line is Greater Than Second Line";
+                   checkLines == -1 ? "First Line is less Than Second Line" :
+                   "First Line is Greater Than Second Line";
         }
         
 
